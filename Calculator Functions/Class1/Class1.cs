bfdbbb2bfaadc0413e1592d0e96c4aa67722fd2d
NoorN <NoorN@NOOR>
2024-12-24 22:05:43 +0300

    Add project files.

---
+++ Calculator Functions/Class1/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_Functions.Class1
+{
+    internal class Class1
+    {
+        public int Num1;
+        public int Num2;
+        public int Num3;
+
+       
+        public int Add(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+
+        
+        public int Add(int number1, int number2, int number3)
+        {
+            return number1 + number2 + number3;
+        }
+
+        
+        public double Add(int number1, int number2, double number3)
+        {
+            return number1 + number2 + number3;
+        }
+
+        
+        public double Salary(double TheSalary, int Hours)
+        {
+            double TheRate = 10;
+            return TheSalary + (Hours * TheRate);
+        }
+    }
+}
+    
+
---
+++ Calculator Functions/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using Calculator_Functions.Class1;
+
+Class1 classInstance = new Class1();
+
+Console.Write("Enter the first number : ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the second number : ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($" the first SUM  is: {classInstance.Add(number1, number2)}");
+
+Console.Write("Enter the third number : ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"The second SUM  is: {classInstance.Add(number1, number2, number3)}");
+
+
+Console.Write("Enter the third number as a DOUBLE: ");
+double number4 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"The third Sum  is: {classInstance.Add(number1, number2, number4)}");
+//calc the salary 
+Console.Write("Enter the salary: ");
+double Salary = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter the working hours: ");
+int Hours = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(classInstance.Salary(Salary, Hours));
+    
